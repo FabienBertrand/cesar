9c43ef819aadc7c7e3ed3f3b1f23af2bcb2ad8a0
raphael <raphael.barral.1@ens.etsmtl.ca>
2024-05-22 15:26:18 -0400

    Hello World

---
+++ menu.py
@@ -0,0 +1,48 @@
+def menu():
+    while True:
+        print('1. utiliser dans la console')
+        print('2. à partir d\'un fichier')
+        print('q. quitter')
+
+        if choix == 'q':
+            break
+
+        depuis_fichier = input().lower() == '2'
+
+
+
+
+def submenu(depuis_fichier):
+    print('1. encrypter')
+    print('2. decrypter')
+    print('3. bruteforce')
+    print('q. retour')
+    choix = input().lower()
+
+    if choix == 'q':
+        return
+    else:
+        submenu()
+
+    if depuis_fichier:
+        filename = input('filename ? \n')
+        try:
+            f = open(filename, 'r')
+        except FileNotFoundError:
+            print('Fichier introuvable')
+            return
+        else:
+            with f:
+                message = f.read()
+    else:
+        message = input('Message ? \n')
+
+    if not choix == '3':
+        clef = input('Clef ? \n')
+
+    if choix == '1':
+        #print(encrypt(message, clef))
+        menu()
+    elif choix == '2':
+        #print(decrypt(message, clef))
+        menu()
